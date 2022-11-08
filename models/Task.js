9750dda84831cab74def1f147ff63250c3ac2af2
Denys Lozynskyi <denislozynskyi@gmail.com>
2022-11-08 17:20:56 +0200

    add done date to task

--- models/Task.js
+++ models/Task.js
@@ -21,6 +21,10 @@ const userSchema = mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'users',
   },
+  doneDate: {
+    type: Date,
+    default: Date.now(),
+  },
 }, {
   timestamps: { createdAt: 'createdDate' },
   versionKey: false,
