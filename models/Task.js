df48f1ddacb9de390ee4d39917d360a0675b359b
Denys Lozynskyi <denislozynskyi@gmail.com>
2022-11-09 10:48:05 +0200

    task model change

--- models/Task.js
+++ models/Task.js
@@ -21,10 +21,6 @@ const userSchema = mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'users',
   },
-  doneDate: {
-    type: Date,
-    default: null,
-  },
 }, {
   timestamps: { createdAt: 'createdDate' },
   versionKey: false,
