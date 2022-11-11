d430ce589e75f20ab2d333c515a9eedd62c828bb
Denys Lozynskyi <denislozynskyi@gmail.com>
2022-11-11 19:13:07 +0200

    fix

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.post('/forgotpassword', forgotPassword);
+router.get('/forgotpassword', forgotPassword);
 
 module.exports = {
   authRouter: router,
